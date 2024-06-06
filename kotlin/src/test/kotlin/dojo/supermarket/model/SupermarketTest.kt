7d6ca3fdf236e902cea8497a9089f2d74d9ed29e
Antoine Alberti <aa@antoinealberti.com>
2024-06-06 14:45:50 +0200

    2 for amount

--- kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
+++ kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import supermarket.model.*
+import supermarket.model.SpecialOfferType.*
 
 class SupermarketTest {
     private lateinit var catalog: FakeCatalog
@@ -41,7 +42,7 @@ class SupermarketTest {
         val cart = ShoppingCart()
         cart.addItemQuantity(toothbrush, 1.0)
 
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0)
+        teller.addSpecialOffer(TenPercentDiscount, toothbrush, 10.0)
 
         val receipt = teller.checksOutArticlesFrom(cart)
 
@@ -57,7 +58,7 @@ class SupermarketTest {
         val cart = ShoppingCart()
         cart.addItemQuantity(toothbrush, 3.0)
 
-        teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 10.0)
+        teller.addSpecialOffer(ThreeForTwo, toothbrush, 10.0)
 
         val receipt = teller.checksOutArticlesFrom(cart)
 
@@ -68,23 +69,55 @@ class SupermarketTest {
         )
     }
 
+    @Test
+    fun `2 for amount`() {
+        val cart = ShoppingCart()
+        cart.addItemQuantity(toothbrush, 3.0)
+
+        teller.addSpecialOffer(TwoForAmount, toothbrush, 1.5)
+
+        val receipt = teller.checksOutArticlesFrom(cart)
+
+        receipt.assertIs(
+            listOf(ReceiptItem(toothbrush, 3.0, 0.99, 2.97)),
+            listOf(Discount(toothbrush, "2 for 1.5", 0.48)),
+            2.49
+        )
+    }
+
+    @Test
+    fun `2 for amount twice`() {
+        val cart = ShoppingCart()
+        cart.addItemQuantity(toothbrush, 4.0)
+
+        teller.addSpecialOffer(TwoForAmount, toothbrush, 1.5)
+
+        val receipt = teller.checksOutArticlesFrom(cart)
+
+        receipt.assertIs(
+            listOf(ReceiptItem(toothbrush, 4.0, 0.99, 3.96)),
+            listOf(Discount(toothbrush, "2 for 1.5", 0.96)),
+            3.0
+        )
+    }
+
     private fun Receipt.assertIs(
         receiptItems: List<ReceiptItem>, discounts: List<Discount>, totalPrice: Double
-    ) {
-        assertAll({
-            assertThatList(this.getItems())
-                .isEqualTo(
-                    receiptItems
-                )
-            assertThatList(this.getDiscounts())
-                .isEqualTo(
-                    discounts
-                )
-            assertThat(this.totalPrice)
-                .usingComparator(DoubleComparator(0.001))
-                .isEqualTo(totalPrice)
-        })
-    }
+    ) = assertAll({
+        assertThatList(this.getItems())
+            .isEqualTo(
+                receiptItems
+            )
+    }, {
+        assertThatList(this.getDiscounts())
+            .isEqualTo(
+                discounts
+            )
+    }, {
+        assertThat(this.totalPrice)
+            .usingComparator(DoubleComparator(0.001))
+            .isEqualTo(totalPrice)
+    })
 
     private fun <T> assertThatList(list: List<T>): IterableAssert<T> =
         assertThat(list)
