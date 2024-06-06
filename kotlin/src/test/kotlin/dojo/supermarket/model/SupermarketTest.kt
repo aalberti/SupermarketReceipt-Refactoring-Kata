18c3c149b798ffa0179db02030bf1e3c2e1f6307
Antoine Alberti <aa@antoinealberti.com>
2024-06-06 14:45:50 +0200

    switch to approval testing

--- kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
+++ kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
@@ -1,19 +1,21 @@
 package dojo.supermarket.model
 
-import org.assertj.core.api.IterableAssert
-import org.assertj.core.api.KotlinAssertions.assertThat
-import org.assertj.core.util.DoubleComparator
+import org.approvaltests.Approvals.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
-import supermarket.model.*
+import supermarket.ReceiptPrinter
+import supermarket.model.Product
+import supermarket.model.ProductUnit
+import supermarket.model.ShoppingCart
 import supermarket.model.SpecialOfferType.*
+import supermarket.model.Teller
 
 class SupermarketTest {
     private lateinit var catalog: FakeCatalog
     private lateinit var teller: Teller
     private val toothbrush = Product("toothbrush", ProductUnit.Each)
     private val apples = Product("apples", ProductUnit.Kilo)
+    private val printer = ReceiptPrinter()
 
     @BeforeEach
     fun setup() {
@@ -29,12 +31,7 @@ class SupermarketTest {
         cart.addItemQuantity(apples, 2.5)
 
         val receipt = teller.checksOutArticlesFrom(cart)
-
-        receipt.assertIs(
-            listOf(ReceiptItem(apples, 2.5, 1.99, 4.975)),
-            listOf(),
-            4.975
-        )
+        verify(printer.printReceipt(receipt))
     }
 
     @Test
@@ -45,12 +42,7 @@ class SupermarketTest {
         teller.addSpecialOffer(TenPercentDiscount, toothbrush, 10.0)
 
         val receipt = teller.checksOutArticlesFrom(cart)
-
-        receipt.assertIs(
-            listOf(ReceiptItem(toothbrush, 1.0, 0.99, 0.99)),
-            listOf(Discount(toothbrush, "10.0% off", 0.099)),
-            0.99 - 0.099
-        )
+        verify(printer.printReceipt(receipt))
     }
 
     @Test
@@ -61,12 +53,7 @@ class SupermarketTest {
         teller.addSpecialOffer(ThreeForTwo, toothbrush, 10.0)
 
         val receipt = teller.checksOutArticlesFrom(cart)
-
-        receipt.assertIs(
-            listOf(ReceiptItem(toothbrush, 3.0, 0.99, 2.97)),
-            listOf(Discount(toothbrush, "3 for 2", 0.99)),
-            0.99 * 2
-        )
+        verify(printer.printReceipt(receipt))
     }
 
     @Test
@@ -77,12 +64,7 @@ class SupermarketTest {
         teller.addSpecialOffer(TwoForAmount, toothbrush, 1.5)
 
         val receipt = teller.checksOutArticlesFrom(cart)
-
-        receipt.assertIs(
-            listOf(ReceiptItem(toothbrush, 3.0, 0.99, 2.97)),
-            listOf(Discount(toothbrush, "2 for 1.5", 0.48)),
-            2.49
-        )
+        verify(printer.printReceipt(receipt))
     }
 
     @Test
@@ -93,37 +75,6 @@ class SupermarketTest {
         teller.addSpecialOffer(TwoForAmount, toothbrush, 1.5)
 
         val receipt = teller.checksOutArticlesFrom(cart)
-
-        receipt.assertIs(
-            listOf(ReceiptItem(toothbrush, 4.0, 0.99, 3.96)),
-            listOf(Discount(toothbrush, "2 for 1.5", 0.96)),
-            3.0
-        )
+        verify(printer.printReceipt(receipt))
     }
-
-    private fun Receipt.assertIs(
-        receiptItems: List<ReceiptItem>, discounts: List<Discount>, totalPrice: Double
-    ) = assertAll({
-        assertThatList(this.getItems())
-            .isEqualTo(
-                receiptItems
-            )
-    }, {
-        assertThatList(this.getDiscounts())
-            .isEqualTo(
-                discounts
-            )
-    }, {
-        assertThat(this.totalPrice)
-            .usingComparator(DoubleComparator(0.001))
-            .isEqualTo(totalPrice)
-    })
-
-    private fun <T> assertThatList(list: List<T>): IterableAssert<T> =
-        assertThat(list)
-            .usingFieldByFieldElementComparator()
-            .usingComparatorForElementFieldsWithNames(
-                DoubleComparator(0.001),
-                "discountAmount", "totalPrice"
-            )
 }
